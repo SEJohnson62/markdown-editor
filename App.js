0d0e34c726ba92ad50f22c132be488cc0a8aeb0c
Susan E. Johnson <seagull62@icloud.com>
2020-01-23 20:53:17 -0800

    initial useState

--- App.js
+++ App.js
@@ -3,8 +3,9 @@ import './App.css';
 import marked from 'marked';
 
 function App() {
+  const [ text, setText ] = useState('First &middot; Second');
   function createMarkup() {
-    return {__html: 'First &middot; Second'};
+    return {__html: text};
   }
 
   function MyComponent() {
